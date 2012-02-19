7684dce85dd283c4a660c67d357b33f72d81bf81
Sascha Peilicke <saschpe@gmx.de>
2012-02-19 23:28:54 +0100

    Follow the Feed 'type' field to 'version' rename in admin config.

--- feeds/admin.py
+++ feeds/admin.py
@@ -24,11 +24,11 @@ class PostInline(admin.StackedInline):
 
 class FeedAdmin(admin.ModelAdmin):
     date_hierarchy = 'updated'
-    list_display = ('url', 'type', 'title', 'subtitle', 'link')
+    list_display = ('url', 'version', 'title', 'subtitle', 'link')
     list_filter  = list_display
     search_fields= list_display
     fieldsets = (
-        (None,               {'fields': ['url', 'type', 'title', 'subtitle', 'link']}),
+        (None,               {'fields': ['url', 'version', 'title', 'subtitle', 'link']}),
         ('Date information', {'fields': ['updated'], 'classes': ['collapse']}),
     )
     inlines = (PostInline,)
