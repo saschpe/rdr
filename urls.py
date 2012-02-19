5e9e30981679c5afa239e31617f180976cbbbcc0
Sascha Peilicke <saschpe@gmx.de>
2012-02-19 22:30:29 +0100

    Some URL toying

--- urls.py
+++ urls.py
@@ -1,17 +1,19 @@
-from django.conf.urls.defaults import patterns, include, url
+# -*- coding: utf-8 -*-
 
-# Uncomment the next two lines to enable the admin:
+from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
+
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'reader.views.home', name='home'),
-    # url(r'^reader/', include('reader.foo.urls')),
+    url(r'^$', 'views.index', name='index'),
+    #url(r'^reader/', include('reader.feeds.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
+    url(r'^feeds/', include('feeds.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
