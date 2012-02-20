d776039b56f527b7c2b931090c45d5e741bc5167
Sascha Peilicke <saschpe@gmx.de>
2012-02-20 20:08:44 +0100

    Add new Feed detail view, update urls

--- feeds/views.py
+++ feeds/views.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 
-from models import Feed, Post
+from models import Feed
 
 from django.http import HttpResponse
 from django.shortcuts import render_to_response, get_object_or_404
 
 
 def index(request):
-    try:
-        feeds = Feed.objects.all().order_by('-updated')[:5]
-    except Poll.DoesNotExist:
-        raise Http404
+    feeds = Feed.objects.all().order_by('-updated')[:20]
     return render_to_response('feeds/index.html', {'feeds': feeds})
+
+def show(request):
+    feed = get_object_or_404(Feed, pk=feed_id)
+    return render_to_response('feeds/show.html', {'feed': feed})
