de45e767bd0606b3305ca62149497b1cc06e2755
Sascha Peilicke <saschpe@gmx.de>
2012-02-21 21:25:40 +0100

    Don't depend on root folder name...

--- settings.py
+++ settings.py
@@ -105,7 +105,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'reader.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
@@ -126,7 +126,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 
-    'reader.feeds',
+    'feeds',
 )
 
 # A sample logging configuration. The only tangible logging
