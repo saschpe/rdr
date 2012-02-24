36981bed53a805f0700c104e620857563714d9cd
Sascha Peilicke <saschpe@gmx.de>
2012-02-25 00:34:48 +0100

    Added remaining accounts urls and a stub 'register' view

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,11 @@ from django.conf.urls.defaults import patterns
 urlpatterns = patterns('',
     (r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'accounts/login.html'}),
     (r'^logout/$', 'django.contrib.auth.views.logout', {'template_name': 'accounts/logout.html'}),
+    (r'^register/$', 'accounts.views.register'),
+    (r'^password_change/$', 'django.contrib.auth.views.password_change', {'template_name': 'accounts/password_change.html'}),
+    (r'^password_change/done/$', 'django.contrib.auth.views.password_change_done', {'template_name': 'accounts/password_change_done.html'}),
+    (r'^password_reset/$', 'django.contrib.auth.views.password_reset', {'template_name': 'accounts/password_reset.html'}),
+    (r'^password_reset/done/$', 'django.contrib.auth.views.password_reset_done', {'template_name': 'accounts/password_reset_done.html'}),
+    (r'^reset/(?P<uidb36>[0-9A-Za-z]{1,13})-(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', 'django.contrib.auth.views.password_reset_confirm', {'template_name': 'accounts/reset.html'}),
+    (r'^reset/done/$', 'django.contrib.auth.views.password_reset_complete', {'template_name': 'accounts/reset_done.html'}),
 )
---
+++ accounts/views.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
+
+
+def register(request):
+    #TODO: Implement
+    return render_to_response('accounts/register.html')
+
+
