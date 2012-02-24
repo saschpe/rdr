b7a90067674f7ff57d362748e808310a1663af33
Sascha Peilicke <saschpe@gmx.de>
2012-02-24 23:21:42 +0100

    Add some initial code for user authentication and populate sidebar

--- feeds/views.py
+++ feeds/views.py
@@ -1,15 +1,21 @@
 # -*- coding: utf-8 -*-
 
-from models import Feed
+from models import Feed, Subscription
 
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
 
 
+@login_required
 def index(request):
-    feeds = Feed.objects.all().order_by('-updated')[:20]
-    return render_to_response('feeds/index.html', {'feeds': feeds})
+    subscriptions = request.user.subscription_set.all()
+    return render_to_response('feeds/index.html', {'subscriptions': subscriptions}, context_instance=RequestContext(request))
 
+
+@login_required
 def show(request, feed_id):
+    subscriptions = request.user.subscription_set.all()
     feed = get_object_or_404(Feed, pk=feed_id)
-    return render_to_response('feeds/show.html', {'feed': feed})
+    return render_to_response('feeds/show.html', {'feed': feed, 'subscriptions': subscriptions}, context_instance=RequestContext(request))
--- urls.py
+++ urls.py
@@ -14,6 +14,7 @@ urlpatterns = patterns('',
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    url(r'^feeds/', include('feeds.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+    (r'^feeds/', include('feeds.urls')),
+    (r'^admin/', include(admin.site.urls)),
+    (r'^accounts/login/$', 'django.contrib.auth.views.login'),
 )
