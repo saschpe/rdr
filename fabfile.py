be89051c507be6a0ab563ce9285f2d286f7763a5
Sascha Peilicke <saschpe@gmx.de>
2012-02-25 19:55:56 +0100

    Way better fabfile.py
    Inspired by
    http://www.robgolding.com/blog/2012/01/14/django-in-production-part-3---automation-and-monitoring/

--- fabfile.py
+++ fabfile.py
@@ -1,110 +1,116 @@
-from fabric.api import abort, cd, local, require, run, settings
+# -*- coding: utf-8 -*-
+
+from fabric.api import abort, cd, env, local, require, run, settings, sudo
 from fabric.contrib.console import confirm
 
-
-# Generic tasks
-
-def hello():
-    print("Hello world!")
+import os
 
 
-def host_type():
-    run('uname -s')
+env.project_name = 'rdr'
+env.server_name = ''
+env.server_user = 'www-data'
+env.server_group = 'www-data'
+env.webapps_root = '/srv/www/webaps/'
+
+env.project_root = os.path.join(env.webapps_root, env.project_name)
+env.activate_script = os.path.join(env.project_root, 'env/bin/active')
+env.wsgi_file = os.path.join(env.project_root, 'django.wsgi')
+env.repo_root = os.path.join(env.project_root, '')
+env.search_index = os.path.join(env.project_root, 'search_index')
+env.requirements_file = os.path.join(env.repo_root, 'requirements.txt')
+env.manage_dir = os.path.join(env.repo_root, env.project_name)
 
 
-# Django tasks
+def production():
+    env.hosts = [env.server_name]
+prod = production
+
+
+def virtualenv(command, use_sudo=False):
+    if use_sudo:
+        func = sudo
+    else:
+        func = run
+    func('source "{0}" && {1}'.format(env.activate_script, command))
+
+
+def update():
+    require('hosts', provided_by=[production])
+    with cd(env.repo_root):
+        run('git pull origin master')
+
+
+def install_requirements():
+    require('hosts', provided_by=[production])
+    virtualenv('pip install -q -r {0}'.format(env.requirements_file))
+
+
+def manage_py(command, use_sudo=False):
+    require('hosts', provided_by=[production])
+    with cd(env.manage_dir):
+        virtualenv('python manage.py {0}'.format(command), use_sudo)
+
 
 def syncdb():
-    '''Run syncdb.'''
-    require('site_path')
-    require('venv_path')
-
-    with cd(env.site_path):
-        run(_python('manage.py syncdb --noinput'))
+    require('hosts', provided_by=[production])
+    manage_py('syncdb --noinput')
 
 
-def collectstatic():
-    '''Collect static media.'''
-    require('site_path')
-    require('venv_path')
+def migrate():
+    require('hosts', provided_by=[production])
+    manage_py('migrate')
 
-    with cd(env.site_path):
-        sudo(_python('manage.py collectstatic --noinput'))
+
+def schema_migration_auto(app=''):
+    require('hosts', provided_by=[production])
+    manage_py('schemamigration --auto {0}'.format(app))
+
+
+def graph_migrations():
+    '''Generate a graphviz .dot file for migrations and convert it to PNG.'''
+    require('hosts', provided_by=[production])
+    manage_py('graphmigrations | dot -Tpng -omigrations.png')
 
 
 def rebuild_index():
-    '''Rebuild the search index.'''
-    require('site_path')
-    require('venv_path')
-    require('process_owner')
-
-    with cd(env.site_path):
-        sudo(_python('manage.py rebuild_index'))
-        sudo('chown -R {0} .xapian'.format(env.process_owner))
+    require('hosts', provided_by=[production])
+    manage_py('rebuild_index --noinput', use_sudo=True)
+    sudo('chown -R {0}:{1} {2}'.format(env.server_user, env.server_group, env.search_index)
 
 
 def update_index():
     '''Update the search index.'''
-    require('site_path')
-    require('venv_path')
-    require('process_owner')
+    require('hosts', provided_by=[production])
+    manage_py('update_index', use_sudo=True)
+    sudo('chown -R {0} .xapian'.format(env.server_user))
 
-    with cd(env.site_path):
-        sudo(_python('manage.py update_index'))
-        sudo('chown -R {0} .xapian'.format(env.process_owner))
+
+def collectstatic():
+    require('hosts', provided_by=[production])
+    manage_py('collectstatic -l --noinput')
+
+
+def reload():
+    require('hosts', provided_by=[production])
+    sudo('supervisorctl status | grep {0} '
+        '| sed "s/.*[pid ]\([0-9]\+\)\,.*/\\1/" '
+        '| xargs kill -HUP'.format(env.project_name))
+
+
+def deploy():
+    require('hosts', provided_by=[production])
+    update()
+    install_requirements()
+    syncdb()
+    migrate()
+    collectstatic()
+    reload()
 
 
 def test(args=''):
-    local('./manage.py test ' + args)
+    manage_py('test ' + args)
    #with settings(warn_only=True):
    #    result = local('./manage.py test ' + args, capture=True)
    #    print result
    #if result.failed and not confirm('Test failed. Continue anyway?'):
    #    abort('Aborting at user request.')
-
-
-# South tasks
-
-def migrate(args=''):
-    '''Run any needed migrations.'''
-    require('site_path')
-    require('venv_path')
-
-    with cd(env.site_path):
-        sudo(_python('manage.py migrate ' + args))
-
-
-def migrate_fake(args=''):
-    '''Run any needed migrations with --fake.'''
-    require('site_path')
-    require('venv_path')
-
-    with cd(env.site_path):
-        sudo(_python('manage.py migrate --fake ' + args))
-
-
-def migrate_reset(args=''):
-    '''Run any needed migrations with --fake.  No, seriously.'''
-    require('site_path')
-    require('venv_path')
-
-    with cd(env.site_path):
-        sudo(_python('manage.py migrate --fake --delete-ghost-migrations ' + args))
-
-
-def schema_migration_auto(app=''):
-    ''''''
-    require('site_path')
-    require('venv_path')
-
-    with cd(env.site_path):
-        sudo(_python('manage.py schemamigration {0} --auto'.format(app)))
-
-
-def graph_migrations():
-    '''Generate a graphviz .dot file for migrations and convert it to PNG.'''
-    require('site_path')
-    require('venv_path')
-
-    with cd(env.site_path):
-        run(_python('manage.py graphmigrations | dot -Tpng -omigrations.png'))
