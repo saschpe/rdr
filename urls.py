7282a0ddc3acb20800ef369e526b6f10ba9d1064
Sascha Peilicke <saschpe@gmx.de>
2012-02-25 01:59:23 +0100

    Add admindocs app

--- urls.py
+++ urls.py
@@ -8,12 +8,8 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     (r'^$', 'views.index'),
-    #url(r'^reader/', include('reader.feeds.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
     (r'^feeds/', include('feeds.urls')),
     (r'^admin/', include(admin.site.urls)),
+    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^accounts/', include('accounts.urls')),
 )
