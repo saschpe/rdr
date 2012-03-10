3b51597d4cd406b1e4b14f8d9973985e77082de2
Sascha Peilicke <saschpe@gmx.de>
2012-03-10 20:47:44 +0100

    Add some tasks to create/update websites.

--- rdr/apps/feeds/tasks.py
+++ rdr/apps/feeds/tasks.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from celery.task import task
-from models import Entry, Feed
+from models import Entry, Feed, Website
 
 
 @task
@@ -16,11 +16,26 @@ def create_feed_from_url(url, create_entries=True):
 
 @task
 def update_feed(feed, update_entries=True):
-    feed.update(update_entries)
-    return feed
+    return feed.update(update_entries)
 
 
 @task
 def update_all_feeds(update_entries=True):
     for feed in Feed.objects.all():
         update_feed.delay(feed, update_entries)
+
+
+@task
+def create_website_from_url(url, create_entries=True):
+    return Website.objects.create_from_url(url, create_entries)
+
+
+@task
+def update_website(website, update_title=False, update_entries=True):
+    return website.update(update_title, update_entries)
+
+
+@task
+def update_all_websites(update_title=False, update_entries=True):
+    for website in Website.objects.all():
+        update_website.delay(website, update_title, update_entries)
