e4f2e8b8553ecbc26fa8774e7c54a6d3d7e219c3
Sascha Peilicke <saschpe@gmx.de>
2012-03-10 20:53:01 +0100

    Update admin interface and fix some 'icontains' lookup errors for foreignkeys and M2M relations.

--- rdr/apps/feeds/admin.py
+++ rdr/apps/feeds/admin.py
@@ -9,35 +9,16 @@ class EntryAdmin(admin.ModelAdmin):
     date_hierarchy = 'updated'
     list_display = ('feed', 'title', 'link', 'author')
     list_filter = ('feed', 'author')
-    search_fields = ('title', 'summary', 'link', 'author')
+    search_fields = ('feed__title', 'title', 'summary', 'link', 'author')
     fieldsets = (
         (None,                  {'fields': ('feed', 'title', 'summary', 'link', 'author')}),
         ('Date information',    {'fields': ('published', 'updated'), 'classes': ('collapse')}),
-        #('Caching information', {'fields': ('etag', 'modified'), 'classes': ('collapse')}),
     )
 
 
 class EntryInline(admin.StackedInline):
     model = Entry
     fieldsets = EntryAdmin.fieldsets
-    extra = 0
-
-
-class FeedAdmin(admin.ModelAdmin):
-    date_hierarchy = 'updated'
-    list_display = ('url', 'title', 'subtitle', 'link')
-    list_filter = ('title', 'link')
-    search_fields = ('url', 'title', 'subtitle', 'link')
-    fieldsets = (
-        (None,               {'fields': ('url', 'title', 'subtitle', 'link')}),
-        ('Date information', {'fields': ('updated',), 'classes': ('collapse')}),
-    )
-    inlines = (EntryInline,)
-
-
-class FeedInline(admin.StackedInline):
-    model = Feed
-    fieldsets = FeedAdmin.fieldsets
     extra = 0
 
 
@@ -48,26 +29,41 @@ class SubscriptionAdmin(admin.ModelAdmin):
 
 
 class SubscriptionInline(admin.StackedInline):
-    models = Subscription
+    model = Subscription
     fieldsets = SubscriptionAdmin.fieldsets
-    extra = 5
+
+
+class FeedAdmin(admin.ModelAdmin):
+    date_hierarchy = 'updated'
+    list_display = ('url', 'title', 'subtitle', 'link', 'website')
+    list_filter = ('title', 'link', 'website')
+    search_fields = ('url', 'title', 'subtitle', 'link', 'website__title')
+    fieldsets = (
+        (None,               {'fields': ('url', 'title', 'subtitle', 'link', 'website')}),
+        ('Date information', {'fields': ('updated',), 'classes': ('collapse')}),
+    )
+    inlines = (EntryInline, SubscriptionInline)
+
+
+class FeedInline(admin.StackedInline):
+    model = Feed
+    fieldsets = FeedAdmin.fieldsets
+    extra = 0
 
 
 class VisitedAdmin(admin.ModelAdmin):
     list_display = ('user', 'entry', 'marked')
-    list_filter = ('user', 'entry')
+    list_filter = ('user', 'entry', 'marked')
     search_fields = ('user__username', 'entry__title')
 
 
 class VisitedInline(admin.StackedInline):
     model = Visited
     fieldsets = VisitedAdmin.fieldsets
-    extra = 5
 
 
 class WebsiteAdmin(admin.ModelAdmin):
     list_display = ('url', 'title')
-    list_filter = ('url', 'title')
     search_fields = ('url', 'title')
     inlines = (FeedInline,)
 
